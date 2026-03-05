7d8c3f13b85997cccbf2eac2efbf2240ac10a48a
BobHell0 <anujan.cenan@gmail.com>
2026-03-05 19:59:58 +1100

    modifed butterworth method to return a full wfdb record now - raw voltage plotting in plotter is now not necessary for plotting

--- src/pre_processing/pre_processor.py
+++ src/pre_processing/pre_processor.py
@@ -4,14 +4,14 @@ import scipy.signal as signal
 import matplotlib.pyplot as plt
 
 from ..data_reader.data_reader import PTB_XL_Reader
-from ..plotter.plotter import Plotter_WFDB
+from ..plotter.plotter import Plotter
 
 
 class Pre_Processor:
     def __init__(self):
         pass
 
-    def apply_butterworth(self, record: wfdb.Record, cutoff_freq=0.5):
+    def apply_butterworth(self, record: wfdb.Record, cutoff_freq=0.5) -> wfdb.Record:
 
         data = record.p_signal
         print(data.shape)
@@ -31,7 +31,13 @@ class Pre_Processor:
             filtered_signal = signal.sosfiltfilt(sos, data_col)
             final_filtered[:,curr_lead] = filtered_signal
 
-        return final_filtered    
+        return wfdb.Record(
+            p_signal=final_filtered,
+            n_sig=number_of_leads,
+            fs=record.fs,
+            units=record.units,
+            sig_name=record.sig_name
+        )
 
     def apply_band_stop_filter(self):
         pass
@@ -43,15 +49,17 @@ if __name__ == "__main__":
     data_reader = PTB_XL_Reader()
     record = data_reader.get_record(record_id=1)
     
-    plotter = Plotter_WFDB()
+    plotter = Plotter()
     plotter.plot_raw_voltages(record.p_signal[:,0])
     plotter.plot_sample(record)
     
     pre_processor = Pre_Processor()
-    filtered_signal = pre_processor.apply_butterworth(record=record)
+
+    filtered_signal_record = pre_processor.apply_butterworth(record=record)
 
 
-    plotter.plot_raw_voltages_mult_leads(filtered_signal, "red")
+    plotter.plot_raw_voltages_mult_leads(filtered_signal_record.p_signal, "red")
+    plotter.plot_sample(filtered_signal_record)
     # for curr_lead in range(12):
     #     plotter.plot_raw_voltages(filtered_signal[:,curr_lead], colour="red")
 
