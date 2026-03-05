ec2a02604b0bd3bfd8eed98aef934aae625ae5b0
BobHell0 <anujan.cenan@gmail.com>
2026-03-05 20:00:42 +1100

    plotter class name changed to just Plotter (not Plotter_WFDB); leverages wfdb plotting for wfdb data and has raw voltages should i ever need to plot raw voltages

--- src/plotter/plotter.py
+++ src/plotter/plotter.py
@@ -7,11 +7,10 @@ import numpy as np
 
 from ..data_reader.data_reader import PTB_XL_Reader
 
-class Plotter_WFDB:
+class Plotter:
     
-    def plot_sample(self, record: wfdb.Record):
-        wfdb.plot_wfdb(record=record, title=f"Record Plot", figsize=(15, 8))
-        return
+    def plot_sample(self, record: wfdb.Record, title="Record Plot"):
+        wfdb.plot_wfdb(record=record, title=title, figsize=(15, 8))
     
     def plot_raw_voltages(self, raw_voltages, colour="blue"):
         plt.figure(figsize=(15, 5))
@@ -32,10 +31,8 @@ class Plotter_WFDB:
 
         plt.show()
 
-
-
 if __name__ == "__main__":
     reader = PTB_XL_Reader()
-    plotter = Plotter_WFDB()
+    plotter = Plotter()
 
     plotter.plot_sample(reader.get_record(record_id=1))
