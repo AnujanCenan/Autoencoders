3069353f794190f0f2adbd058c827eceeaf817b2
BobHell0 <anujan.cenan@gmail.com>
2026-03-05 20:07:20 +1100

    got rid of superfluous print statements

--- src/data_reader/data_reader.py
+++ src/data_reader/data_reader.py
@@ -35,18 +35,14 @@ class PTB_XL_Reader(Data_Reader):
     def get_record(self, record_id: int):
         Y = pd.read_csv(self.database_path + 'ptbxl_database.csv', index_col='ecg_id')
 
-        print(Y.loc[1, "filename_lr"])
         filename = Y.loc[record_id, "filename_lr"]
-        print(self.database_path + filename)
 
         record = wfdb.rdrecord(self.database_path + filename)
         return record
 
     def num_records(self) -> int:
         Y = pd.read_csv(self.database_path + 'ptbxl_database.csv', index_col='ecg_id')
-        print(Y.shape[0])
         return Y.shape[0]
 
     def get_record_values(self, record: wfdb.Record | wfdb.MultiRecord):
-        print(record.p_signal)
         return record.p_signal
